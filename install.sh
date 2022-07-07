4508da791737dd0e81d05c68844005f7e5d634d9
John Michael C. Tejano <39812878+johntejano2241@users.noreply.github.com>
2022-07-07 20:30:14 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,42 @@
+apt-get update
+apt-get upgrade -y
+##Download script
+mkdir /etc/scripts/
+wget -O /etc/scripts/script.py https://raw.githubusercontent.com/tunneler123/sshplus/master/script.py
+##Screen
+apt-get install screen -y
+##dropbearsetup
+apt-get install dropbear -y
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=442/DROPBEAR_PORT=442/g' /etc/default/dropbear
+service dropbear restart
+##python install
+apt-get install python -y
+##SSL
+apt-get -y install stunnel4
+wget -O /etc/stunnel/stunnel.pem "https://raw.githubusercontent.com/tunneler123/sslgtm/main/stunnel.pem"
+wget -O /etc/stunnel/stunnel.conf "https://raw.githubusercontent.com/tunneler123/sslgtm/main/stunnel.conf"
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+service stunnel4 restart
+###Work
+chmod a+x script.py
+cd /usr/bin
+wget -O account https://raw.githubusercontent.com/tunneler123/sshplus/master/account.sh
+screen -dmS screen python ./script.py
+cat <<EOF >>start
+screen -dmS screen python /etc/scripts/script.py
+EOF
+cat <<EOF >>stop
+pkill python
+EOF
+chmod +x start
+chmod +x stop
+chmod +x account
+###TUNNELER###
+start
+clear
+echo -e "\e[1;32m PHTUNNELER AUTOSCRIPT \e[0m"
+echo -e "\e[1;32m SSH INSTALLED DONE \e[0m"
+echo -e "\e[1;32m DEFAULT WS PORT IS 80 \e[0m"
+echo -e "\e[1;32m DEFAULT SSH PORT IS 442 \e[0m"
+echo -e "\e[1;32m type "account" to add user \e[0m"
